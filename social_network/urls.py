3189b1c1e4318cc5082c6083f5c0c57d83a2d1b2
KirillMelanich <kirillmelanich@gmail.com>
2023-12-11 14:53:40 +0200

    Connected Debug_Toolbar

--- social_network/urls.py
+++ social_network/urls.py
@@ -19,5 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/user/", include("user.urls", namespace="user"))
+    path("api/user/", include("user.urls", namespace="user")),
+    path("api/tweetbook/", include("tweetogram.urls", namespace="tweetogram")),
+
+    path("__debug__/", include("debug_toolbar.urls"))
 ]
