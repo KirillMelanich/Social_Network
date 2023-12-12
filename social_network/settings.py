47fef8621e97c8ea1f82a0f3e6925b0d332d318b
KirillMelanich <kirillmelanich@gmail.com>
2023-12-12 20:12:22 +0200

    Sett JWT authentication, drf_spectacular documentation and permissions

--- social_network/settings.py
+++ social_network/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -41,8 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "debug_toolbar",
+    "drf_spectacular",
     "rest_framework",
+    'rest_framework_simplejwt',
+    "debug_toolbar",
     "tweetogram",
     "user",
 ]
@@ -131,3 +133,37 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "1000/day", "user": "3000/day"},
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
+
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Social network",
+    "DESCRIPTION": "Make posts, like and dislike them!",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+    "SWAGGER_UI_SETTINGS": {
+        "deepLinking": True,
+        "defaultModelRendering": "model",
+        "defaultModelsExpandDepth": 2,
+        "defaultModelExpandDepth": 2,
+    },
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=5),  # in commercial project it must be 5min
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),  # in commercial project it must be 7days
+    "ROTATE_REFRESH_TOKENS": False,
+}
--- tweetogram/models.py
+++ tweetogram/models.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.db import models
-from django.db.models import Max, F
+from django.db.models import Max
 
 
 class Post(models.Model):
